19e82e76a71dfa3b134916931198592fdcae7139
apenasopedro <pedroleon.pc@hotmail.com>
2023-01-02 16:32:37 -0300

    test

--- button.js
+++ button.js
@@ -1,15 +1,15 @@
-let resp = document.getElementById('res')
+var resp = document.querySelector('div#res')
 
 function design(){
     resp.style.color="white"
     resp.style.backgroundColor="green"
     resp.style.borderRadius="10px"
-    resp.style.maxWidth="120px"
+    resp.style.maxWidth="190px"
     resp.style.padding="7px"
     resp.style.margin="auto"
     resp.style.textAlign="center"
     resp.style.marginTop="20px"
-    
+    event.preventDefault()
 }
 function error(){
     design()
@@ -19,32 +19,30 @@ function error(){
 function logar(){
     let input = document.querySelector('div.user .form').value;
     let input_pass = document.querySelector('div.user .form-pass').value;
-    
-    let button = document.getElementById('button')
-    let resp = document.getElementById('res')
-    if(input.length == 0 || input_pass.length ==0 ){
+        if(input.length == 0 || input_pass.length ==0 ){
         resp.innerHTML=('Digite Corretamente os Dados!')
         error()
         window.alert("Você precisa digitar seus dados!")
     }else if (input.length > 1 || input_pass.length > 1 ){
         resp.innerHTML=("Você está logado!")
         design()
-        event.preventDefault()
+        
         
     }
 }
 function cadastrar(){
-    
+    let input = document.querySelector('div.user .form').value;
+    let input_pass = document.querySelector('div.user .form-pass').value;
     let input_name = document.querySelector('div.user .form-user').value;
     let input_user = document.querySelector('div.user .form-name').value;
-    if(input_name.length==0 || input_user.length==0){
+    if(input_name.length==0 || input_user.length==0 || input.length == 0 || input_pass.length ==0 ){
         resp.innerHTML=('Digite Corretamente os Dados!')
         error()
         window.alert("Você precisa digitar seus dados!")
-    }else if (input_name.length > 1 || input_user.length > 1){
+    }else if (input_name.length > 1 || input_user.length > 1 || input.length > 1 || input_pass.length > 1 ){
         resp.innerHTML=("Você está Cadastrado!")
         design()
-        event.preventDefault()
+       
         
-    }        
+    }
 }
